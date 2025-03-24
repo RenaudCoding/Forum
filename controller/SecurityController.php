02966ff0e99051ba3957374ff9568491d436a7ec
Renaud Laenger <renaudlaenger@wanadoo.fr>
2025-03-24 11:25:09 +0100

    ajout d'un formulaire d'inscription

--- controller/SecurityController.php
+++ controller/SecurityController.php
@@ -7,7 +7,15 @@ use App\ControllerInterface;
 class SecurityController extends AbstractController{
     // contiendra les méthodes liées à l'authentification : register, login et logout
 
-    public function register () {}
+    public function register () {
+        
+        return [
+            "view" => VIEW_DIR."forum/register.php",
+            "meta_description" => "Inscription"
+        ];
+    }
+
     public function login () {}
+
     public function logout () {}
 }
---
+++ view/forum/register.php
@@ -0,0 +1,23 @@
+<h1>Inscription</h1>
+
+<form action="index.php?ctrl=home&action=addUser" method="post">
+    <p>
+        <label>Pseudo :
+            <input type="text" name="nickname">
+        </label>
+    </p>
+    <p>
+        <label>Email :
+            <input type="text" name="email">
+        </label>
+    </p>
+    <p>
+        <label>Mot de passe :
+            <input type="text" name="password">
+        </label>
+    </p>
+    <p>
+        <input type="submit" value="S'inscrire">
+    </p>
+</form>
+<a href="index.php?ctrl=forum&action=index">Retour</a>
